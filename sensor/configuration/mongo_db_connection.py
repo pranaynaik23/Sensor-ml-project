a5b0aa64dab7bd16a6f4fa5ab4df3ef1a3f3d449
pranaynaik23 <pranaynaik007@hotmail.com>
2023-01-11 15:33:28 +0530

    modified mongodb connection

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -6,13 +6,17 @@ from sensor.constant.env_variable import MONGODB_URL_KEY
 ca = certifi.where()
 
 class MongoDBClient:
-    client = None
-    def __init__(self, database_name=DATABASE_NAME) -> None:
+     client = None
+     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv("MONGODB_URL_KEY")
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
